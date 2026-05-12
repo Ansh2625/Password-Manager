d48712a976bd7f07e233fcafa0fa7c4a6b7259fb
Ansh2625 <anshsharma2625@gmail.com>
2026-05-12 15:33:41 +0530

    Updated the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
 
-    <nav className=' text-violet-100 flex md:flex-row flex-col gap-5 justify-between p-4 text-xl font-bold items-center'>
+    <nav className=' text-violet-100 flex md:flex-row flex-col gap-5 justify-center p-4 text-xl font-bold items-center'>
 
         <div className='px-5'>
           <img src="favicon.png" alt="logo" width={70}/>
@@ -15,8 +15,6 @@ const Navbar = () => {
             <li className='hover:text-fuchsia-400 cursor-pointer'><NavLink to="/about">About</NavLink></li>
             <li className='hover:text-fuchsia-400 cursor-pointer'><NavLink to="/contact">Contact</NavLink></li>
         </ul>
-
-        <button className="bg-fuchsia-600 hover:bg-fuchsia-500 px-4 py-2 rounded-xl">Login</button>
 
     </nav>
 
